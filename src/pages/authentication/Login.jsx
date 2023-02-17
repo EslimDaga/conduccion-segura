dee8a1eb973c274dc3a951c3533e7086b9de465a
Eslim Daga <eslimdaga@gmail.com>
2023-02-17 10:09:12 -0500

    feat:📝:La validación funciona correctamente, queda agregarle la funcionalidad para el inicio de sesión

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -1,4 +1,23 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 const Login = () => {
+	const formik = useFormik({
+		initialValues: {
+			username: "",
+			password: "",
+		},
+		validationSchema: Yup.object({
+			username: Yup.string().required("El nombre de usuario es obligatorio"),
+			password: Yup.string()
+				.required("La contraseña es obligatoria")
+				.min(6, "La contraseña debe tener al menos 6 caracteres"),
+		}),
+		onSubmit: formData => {
+			console.log(formData);
+		},
+	});
+
 	return (
 		<section className="flex flex-col md:flex-row h-screen items-center">
 			<div className="hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
@@ -18,26 +37,37 @@ const Login = () => {
 						alt="logo-solgas"
 						className="w-3/4 h-3/4 mx-auto"
 					/>
-					<h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
+					<h1 className="text-xl md:text-2xl font-bold leading-tight my-6">
 						Que bueno verte de nuevo! 😃
 					</h1>
-					<form className="mt-6" action="#" method="POST">
+					<form onSubmit={formik.handleSubmit}>
 						<div>
 							<label
 								htmlFor="email"
 								className="block text-gray-700 font-medium"
 							>
-								Correo Electronico
+								Nombre de usuario
 							</label>
 							<input
-								id="email"
-								type="email"
-								placeholder="Ingrese su correo electronico"
-								className="w-full px-4 py-4 rounded-xl bg-gray-200 mt-2 border-2 focus:border-solgas-primary focus:bg-white focus:outline-none"
-								autoFocus
+								id="username"
+								type="username"
+								placeholder="Ingrese su usuario"
+								className={
+									"w-full px-4 py-5 rounded-xl bg-gray-200 mt-2 border-2 focus:border-solgas-primary focus:bg-white focus:outline-none" +
+									(formik.touched.username && formik.errors.username
+										? " border-red-500 focus:border-red-500"
+										: "")
+								}
 								autoComplete="false"
-								required
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.username}
 							/>
+							{formik.touched.username && formik.errors.username ? (
+								<div className="pt-2 text-red-500 text-sm font-medium">
+									{formik.errors.username}
+								</div>
+							) : null}
 						</div>
 						<div className="mt-4">
 							<label
@@ -50,17 +80,68 @@ const Login = () => {
 								id="password"
 								type="password"
 								placeholder="Ingrese su contraseña"
-								className="w-full px-4 py-4 rounded-xl bg-gray-200 mt-2 border-2 focus:border-solgas-primary
-                  focus:bg-white focus:outline-none"
-								required
+								className={
+									"w-full px-4 py-5 rounded-xl bg-gray-200 mt-2 border-2 focus:border-solgas-primary focus:bg-white focus:outline-none" +
+									(formik.touched.password && formik.errors.password
+										? " border-red-500 focus:border-red-500"
+										: "")
+								}
+								autoComplete="false"
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								value={formik.values.password}
 							/>
+							{formik.touched.password && formik.errors.password ? (
+								<div className="pt-2 text-red-500 text-sm font-medium">
+									{formik.errors.password}
+								</div>
+							) : null}
 						</div>
 						<button
 							type="submit"
-							className="w-full block bg-solgas-primary hover:bg-solgas-secondary focus:bg-blue-400 text-white font-semibold rounded-xl
-                px-4 py-4 mt-6"
+							className={
+								"w-full block bg-solgas-primary hover:bg-solgas-primary-dark text-white font-semibold rounded-xl px-4 py-5 mt-6 transition duration-500 ease select-none focus:outline-none focus:shadow-outline" +
+								(formik.isSubmitting ||
+								formik.values.username === "" ||
+								formik.values.password === ""
+									? " flex justify-center items-center opacity-50 cursor-not-allowed"
+									: formik.isValid
+									? " hover:bg-solgas-secondary"
+									: " hover:bg-solgas-primary")
+							}
+							disabled={
+								formik.isSubmitting || !formik.isValid
+									? true
+									: false ||
+									  formik.values.username === "" ||
+									  formik.values.password === ""
+									? true
+									: false
+							}
 						>
-							Iniciar Sesión
+							{formik.isSubmitting ? "Iniciando sesión ..." : "Iniciar Sesión"}
+							{formik.isSubmitting && (
+								<svg
+									className="animate-spin -mr-1 ml-3 h-5 w-5 text-white"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+								>
+									<circle
+										className="opacity-25"
+										cx="12"
+										cy="12"
+										r="10"
+										stroke="currentColor"
+										strokeWidth="4"
+									></circle>
+									<path
+										className="opacity-75"
+										fill="currentColor"
+										d="M4 12a8 8 0 018-8v1a7 7 0 00-7 7h1z"
+									></path>
+								</svg>
+							)}
 						</button>
 					</form>
 					<hr className="my-6 border-gray-300 w-full" />
