776c3b953433200e86ac70cc30b800ae01e298dc
Eslim Daga <esliim9811@gmail.com>
2023-06-15 11:11:32 -0500

    update:➕:Eh agregado mejores vistas para el modal en cuanto a visuzlizar la data, solo falta agregar las fotografías.

--- src/pages/initial-inspections/InitialInspections.jsx
+++ src/pages/initial-inspections/InitialInspections.jsx
@@ -14,6 +14,7 @@ const InitialInspections = () => {
   const gridRef = useRef();
   const dispatch = useDispatch();
 
+  const [openTab, setOpenTab] = useState(1);
   const [position, setPosition] = useState([0, 0])
   const [showModalViewInitialInspection, setShowModalViewInitialInspection] = useState(false);
 
@@ -26,6 +27,12 @@ const InitialInspections = () => {
 
   const columnDefs = useMemo(() => {
     return [
+      {
+        field: "unit_name",
+        filter: true,
+        headerName: "Unidad",
+        cellStyle: { textAlign: "center" },
+      },
       {
         field: "driver_id_number",
         filter: true,
@@ -118,8 +125,11 @@ const InitialInspections = () => {
     const marker = new L.marker(coords);
     marker.addTo(map);
 
-    //Add popup to marker
-    marker.bindPopup(initialInspection?.unit_name).openPopup();
+    marker.bindPopup(initialInspection?.unit_name)
+
+    setTimeout(() => {
+      map.invalidateSize()
+    }, 0)
     return null;
   }
 
@@ -135,44 +145,247 @@ const InitialInspections = () => {
 
   return (
     <>
-      {
-        showModalViewInitialInspection && (
-          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 py-5 flex items-center justify-center z-50">
-            <div className="bg-white rounded-xl overflow-auto max-w-3xl w-full max-h-full">
-              <div className="z-50 rounded-t-xl bg-white-primary flex items-start justify-between px-6 py-5 max-w-3xl w-full border-b-2 border-white-secondary dark:border-dark-secondary">
-                <h3 className="text-gray-900 dark:text-gray-100 font-medium self-center">
-                  Inspección inicial
-                </h3>
-                <button
-                  onClick={closeModalViewInitialInspection}
+      {showModalViewInitialInspection && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 py-5 flex items-center justify-center z-50">
+          <div className="bg-white rounded-xl overflow-auto max-w-3xl w-full max-h-full">
+            <div className="z-50 rounded-t-xl bg-white-primary flex items-start justify-between px-6 py-5 max-w-3xl w-full border-b-2 border-white-secondary dark:border-dark-secondary">
+              <h3 className="text-gray-900 dark:text-gray-100 font-medium self-center">
+                Inspección inicial
+              </h3>
+              <button
+                onClick={closeModalViewInitialInspection}
+                className={
+                  "p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                }
+              >
+                <span
                   className={
-                    "p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    "bg-transparent text-gray-500 hover:text-gray-900 dark:text-gray-300 hover:dark:text-gray-100 h-6 w-6 text-xl outline-none focus:outline-none"
                   }
                 >
-                  <span
-                    className={
-                      "bg-transparent text-gray-500 hover:text-gray-900 dark:text-gray-300 hover:dark:text-gray-100 h-6 w-6 text-xl outline-none focus:outline-none"
-                    }
-                  >
-                    <XCircleIcon className="w-6 h-6" />
-                  </span>
-                </button>
-              </div>
-              <div className="p-6">
-                {initialInspection?.driver_fullname}
-                <MapContainer center={position} zoom={13} scrollWheelZoom={true} className="w-full h-96">
-                  <TileLayer
-                    attribution='&copy; <a href="https://maps.google.com/">Google Maps</a>'
-                    url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
-                    subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
-                  />
-                  <SetViewOnClick coords={position} />
-                </MapContainer>
-              </div>
+                  <XCircleIcon className="w-6 h-6" />
+                </span>
+              </button>
+            </div>
+            <div className="p-6">
+              <>
+                <div className="flex flex-wrap">
+                  <div className="w-full">
+                    <ul
+                      className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
+                      role="tablist"
+                    >
+                      <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                        <a
+                          className={
+                            "text-sm font-bold px-5 py-3 shadow-lg rounded block leading-normal " +
+                            (openTab === 1
+                              ? "text-white bg-solgas-primary"
+                              : "text-white-600 bg-white")
+                          }
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setOpenTab(1);
+                          }}
+                          data-toggle="tab"
+                          href="#link1"
+                          role="tablist"
+                        >
+                          Información
+                        </a>
+                      </li>
+                      <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                        <a
+                          className={
+                            "text-sm font-bold px-5 py-3 shadow-lg rounded block leading-normal " +
+                            (openTab === 2
+                              ? "text-white bg-solgas-primary"
+                              : "text-white-600 bg-white")
+                          }
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setOpenTab(2);
+                          }}
+                          data-toggle="tab"
+                          href="#link2"
+                          role="tablist"
+                        >
+                          Preguntas
+                        </a>
+                      </li>
+                      <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                        <a
+                          className={
+                            "text-sm font-bold px-5 py-3 shadow-lg rounded block leading-normal " +
+                            (openTab === 3
+                              ? "text-white bg-solgas-primary"
+                              : "text-white-600 bg-white")
+                          }
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setOpenTab(3);
+                          }}
+                          data-toggle="tab"
+                          href="#link3"
+                          role="tablist"
+                        >
+                          Mapa
+                        </a>
+                      </li>
+                    </ul>
+                    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+                      <div className="px-4 py-5 flex-auto">
+                        <div className="tab-content tab-space">
+                          <div
+                            className={openTab === 1 ? "block" : "hidden"}
+                            id="link1"
+                          >
+                            <div className="relative flex h-full flex-col overflow-hidden bg-white text-gray-600">
+                              <div className="flex-auto p-6">
+                                <div className="relative flex flex-col justify-center">
+                                  <div className="absolute left-4 h-full border-r-2"></div>
+                                  <div className="relative mb-4">
+                                    <span className="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                      U
+                                    </span>
+                                    <div className="ml-12 w-auto py-2">
+                                      <h6 className="text-sm font-semibold text-blue-900">
+                                        {initialInspection?.unit_name}
+                                      </h6>
+                                    </div>
+                                  </div>
+                                  <div className="relative mb-4">
+                                    <span className="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                      D
+                                    </span>
+                                    <div className="ml-12 w-auto py-2">
+                                      <h6 className="text-sm font-semibold text-blue-900">
+                                        {initialInspection?.driver_id_number}
+                                      </h6>
+                                    </div>
+                                  </div>
+                                  <div className="relative mb-4">
+                                    <span className="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                      N
+                                    </span>
+                                    <div className="ml-12 w-auto py-2">
+                                      <h6 className="text-sm font-semibold text-blue-900">
+                                        {initialInspection?.driver_fullname}
+                                      </h6>
+                                    </div>
+                                  </div>
+                                  <div className="relative mb-4">
+                                    <span className="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                      L
+                                    </span>
+                                    <div className="ml-12 w-auto py-2">
+                                      <h6 className="text-sm font-semibold text-blue-900">
+                                        {initialInspection?.latitude}
+                                      </h6>
+                                    </div>
+                                  </div>
+                                  <div className="relative mb-4">
+                                    <span className="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                      L
+                                    </span>
+                                    <div className="ml-12 w-auto py-2">
+                                      <h6 className="text-sm font-semibold text-blue-900">
+                                        {initialInspection?.longitude}
+                                      </h6>
+                                    </div>
+                                  </div>
+                                  <div className="relative mb-4">
+                                    <span className="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                      T
+                                    </span>
+                                    <div className="ml-12 w-auto py-2">
+                                      <h6 className="text-sm font-semibold text-blue-900">
+                                        {initialInspection?.duration_time}{" "}
+                                        segundos
+                                      </h6>
+                                    </div>
+                                  </div>
+                                  <div className="relative mb-4">
+                                    <span className="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                      M
+                                    </span>
+                                    <div className="ml-12 w-auto py-2">
+                                      <h6 className="text-sm font-semibold text-blue-900">
+                                        {initialInspection?.modified}
+                                      </h6>
+                                    </div>
+                                  </div>
+                                  <div className="relative">
+                                    <span className="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                      C
+                                    </span>
+                                    <div className="ml-12 w-auto py-2">
+                                      <h6 className="text-sm font-semibold text-blue-900">
+                                        {initialInspection?.created}
+                                      </h6>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div
+                            className={openTab === 2 ? "block" : "hidden"}
+                            id="link2"
+                          >
+                            <div className="relative flex h-full flex-col overflow-hidden bg-white text-gray-600">
+                              <div className="flex-auto p-6">
+                                <div className="relative flex flex-col justify-center">
+                                  <div className="absolute left-4 h-full border-r-2"></div>
+                                  {initialInspection?.questions.map(
+                                    (question, index) => (
+                                      <div class="relative mb-4">
+                                        <span class="absolute inline-flex h-6 w-6 items-center justify-center rounded-full bg-solgas-primary p-4 text-center text-base font-semibold text-white shadow">
+                                          C
+                                        </span>
+                                        <div class="ml-12 w-auto pt-1">
+                                          <h6 class="text-sm font-semibold text-blue-900">
+                                            {question?.question}
+                                          </h6>
+                                          <p class="mt-1 text-xs text-gray-500">
+                                            {question?.answer}
+                                          </p>
+                                        </div>
+                                      </div>
+                                    )
+                                  )}
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div
+                            className={openTab === 3 ? "block" : "hidden"}
+                            id="link3"
+                          >
+                            <MapContainer
+                              center={position}
+                              zoom={13}
+                              scrollWheelZoom={true}
+                              className="w-full h-96"
+                            >
+                              <TileLayer
+                                attribution='&copy; <a href="https://maps.google.com/">Google Maps</a>'
+                                url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+                                subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                              />
+                              <SetViewOnClick coords={position} />
+                            </MapContainer>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
             </div>
           </div>
-        )
-      }
+        </div>
+      )}
       <main className="h-screen pt-20">
         <div className="pt-6 w-full leading-6">
           <div className="mx-auto px-4 sm:px-6 lg:px-8">
