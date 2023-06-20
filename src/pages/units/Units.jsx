97a74aead1b91fcc9d1c0935b6b96ef1693c3bd2
Eslim Daga <esliim9811@gmail.com>
2023-06-20 10:40:26 -0500

    bug:🐛:Eh cambiado el class por el classname que ocasionaban errores.

--- src/pages/units/Units.jsx
+++ src/pages/units/Units.jsx
@@ -708,11 +708,11 @@ const Units = () => {
               </p>
               <button
                 onClick={showAddUnitModal}
-                class="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-solgas-primary rounded-md cursor-pointer group ring-offset-2 ring-1 ring-solgas-secondary ring-offset-indigo-200 hover:ring-offset-solgas-primary ease focus:outline-none"
+                className="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-solgas-primary rounded-md cursor-pointer group ring-offset-2 ring-1 ring-solgas-secondary ring-offset-indigo-200 hover:ring-offset-solgas-primary ease focus:outline-none"
               >
-                <span class="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                <span class="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                <span class="relative z-20 flex items-center text-sm">
+                <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+                <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+                <span className="relative z-20 flex items-center text-sm">
                   Agregar unidad
                   <PlusCircleIcon className="h-5 w-5 ml-2" />
                 </span>
