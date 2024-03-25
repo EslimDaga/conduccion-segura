d12a20e0f9c3d77827a06cc290dd238a16ef545d
Eslim Daga <eslimdaga@gmail.com>
2024-03-25 16:05:44 -0500

    Cambiando el nombre del endpoint

--- src/services/authenticationService.jsx
+++ src/services/authenticationService.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export const loginService = async credentials => {
-	const response = await axios.post(
-		"http://sfdev.segursat.com/web/api/token/obtain/",
-		{
-			username: credentials.username,
-			password: credentials.password,
-		}
-	);
-	return response.data;
+export const loginService = async (credentials) => {
+  const response = await axios.post(
+    "http://safedriving.segursat.com/web/api/token/obtain/",
+    {
+      username: credentials.username,
+      password: credentials.password,
+    }
+  );
+  return response.data;
 };
--- src/services/jwtInterceptor.jsx
+++ src/services/jwtInterceptor.jsx
@@ -1,52 +1,52 @@
 import axios from "axios";
 
 const jwtInterceptor = axios.create({
-	baseURL: "http://sfdev.segursat.com/web/api",
-	headers: {
-		Authorization: "JWT ",
-	},
+  baseURL: "http://safedriving.segursat.com/web/api",
+  headers: {
+    Authorization: "JWT ",
+  },
 });
 
 jwtInterceptor.interceptors.request.use(
-	config => {
-		let token = JSON.parse(localStorage.getItem("user"));
-		config.headers.Authorization = `JWT ${token.access}`;
-		return config;
-	},
-	error => {
-		Promise.reject(error);
-	}
+  (config) => {
+    let token = JSON.parse(localStorage.getItem("user"));
+    config.headers.Authorization = `JWT ${token.access}`;
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
 );
 
 jwtInterceptor.interceptors.response.use(
-	response => {
-		return response;
-	},
-	async error => {
-		if (error.response.status === 401) {
-			localStorage.removeItem("user");
-			window.location = "/login";
-		}
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("user");
+      window.location = "/login";
+    }
 
-		if (error.response.status === 403) {
-			const authData = JSON.parse(localStorage.getItem("user"));
-			const payload = {
-				access: authData.access,
-				refresh: authData.refresh,
-			};
+    if (error.response.status === 403) {
+      const authData = JSON.parse(localStorage.getItem("user"));
+      const payload = {
+        access: authData.access,
+        refresh: authData.refresh,
+      };
 
-			let apiResponse = await jwtInterceptor.post("/token/refresh/", payload);
+      let apiResponse = await jwtInterceptor.post("/token/refresh/", payload);
 
-			localStorage.setItem(
-				"user",
-				JSON.stringify(Object.assign(authData, apiResponse.data))
-			);
-			error.config.headers["Authorization"] = `JWT ${apiResponse.data.access}`;
-			return axios(error.config);
-		} else {
-			return Promise.reject(error);
-		}
-	}
+      localStorage.setItem(
+        "user",
+        JSON.stringify(Object.assign(authData, apiResponse.data))
+      );
+      error.config.headers["Authorization"] = `JWT ${apiResponse.data.access}`;
+      return axios(error.config);
+    } else {
+      return Promise.reject(error);
+    }
+  }
 );
 
 export default jwtInterceptor;
